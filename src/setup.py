ad7f50a5df977d01f1bd093cd67440473a2ee840
grromrell <grromrell@gmail.com>
2014-04-18 20:24:09 -0500

    Initial Commit

---
+++ src/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 'data_odbc',
+    version = '0.3',
+    description = 'Access MS SQL Server with Python',
+    author = 'Ryan Brunt, Greg Romrell',
+    author_email = 'ryanbrunt@savvysherpa.com, gregromrell@savvysherpa.com',
+    py_modules=['data_odbc']
+)
