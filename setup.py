c67045037ee635f0e4308850e2215c717dc39bfb
Greg Romrell <grromrell@gmail.com>
2017-11-08 19:11:37 -0700

    Update to new name

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(
-    name = 'data_odbc',
+    name = 'db_to_pandas',
     version = '0.1',
     description = 'Database operation with Pandas',
     author = 'Ryan Brunt, Greg Romrell',
     author_email = 'grromrell@gmail.com',
-    py_modules=['data_odbc']
+    py_modules=['db_to_pandas']
 )
